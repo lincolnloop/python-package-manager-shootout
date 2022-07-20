111f920d4c914a4ca08fe043e9a07bbe106d2a32
Peter Baumgartner <pete@lincolnloop.com>
2022-07-20 10:41:21 -0600

    Logging in stats command

--- bin/generate_average_stats.sh
+++ bin/generate_average_stats.sh
@@ -5,16 +5,19 @@ RUN_COUNT=6
 RECENT_RUNS=$(gh run list --workflow benchmark --json databaseId,event --limit 100 --jq 'map(select(.event=="schedule")) | .[].databaseId' | head -n "$RUN_COUNT")
 # concatenate the stats files into a single file
 for id in $RECENT_RUNS; do
+    echo "Downloading stats from run $id"
     gh run download "$id" --name stats
     cat stats.csv >> tmp.csv
     rm -f stats.csv stats.db
 done
 
 # strip extra headers from the stats file
+echo "Joining stats files"
 HEADER=$(head -n 1 tmp.csv)
 echo "$HEADER" > full.csv
 grep -v "$HEADER" tmp.csv >> full.csv
 
+echo "Calculating average stats across runs"
 # average stats by tool/stat
 sqlite-utils memory full.csv \
   "SELECT
@@ -26,3 +29,4 @@ sqlite-utils memory full.csv \
   FROM full
   GROUP BY tool,stat" \
   --csv > stats.csv
+echo "Stats saved to stats.csv"
