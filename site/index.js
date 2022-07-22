7447c16eb5030e2ae877144279d55e44a33487c7
Peter Baumgartner <pete@lincolnloop.com>
2022-07-21 19:41:40 -0600

    more colors available for graph
    With potentially showing two versions at once, we may need more

--- site/index.js
+++ site/index.js
@@ -20,7 +20,20 @@ function newShade(hexColor, magnitude) {
   }
 }
 
-const colors = ["#b3cde3", "#ccebc5", "#decbe4", "#fed9a6", "#ffffcc"];
+const colors = [
+  "#8dd3c7",
+  "#ffffb3",
+  "#bebada",
+  "#fb8072",
+  "#80b1d3",
+  "#fdb462",
+  "#b3de69",
+  "#fccde5",
+  "#d9d9d9",
+  "#bc80bd",
+  "#ccebc5",
+  "#ffed6f",
+];
 const max = Math.ceil(data.max) + 20;
 delete data.max;
 for (const graph in data) {
